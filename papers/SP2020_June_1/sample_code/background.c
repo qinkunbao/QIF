c47059616e8937d9d1d164c522a87f8bd81c6d50
Qinkun Bao <qinkunbao@gmail.com>
2019-06-04 13:27:34 +0200

    update the paper

---
+++ papers/SP2020_June_1/sample_code/background.c
@@ -0,0 +1,20 @@
+unsigned char* sample_encrypt_setup
+               (encrypt_ctx *ctx,
+               const unsigned char *key)
+{
+    int i, j, k;
+    unsigned char *m, T[256];
+    m = ctx->m;
+    for(i = 0 ; i < 256; i++, k++)
+    {
+        // Secret Dependent Memory Access
+        m[i] = Table[key[k] % 256];
+        // Secret Dependent Control Flow Transfer
+        if(key[k] == 0)
+        {
+            m[i] = k * key[i];
+            m[i] = m[i] % 256;
+        }
+    }
+
+}
