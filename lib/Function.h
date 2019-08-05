87dc962dda57197f7e7b729f18cfa440ffc3062a
Qinkun Bao <qinkunbao@gmail.com>
2019-08-05 00:55:54 -0400

    update QIF

--- lib/Function.h
+++ lib/Function.h
@@ -18,13 +18,25 @@
 
 namespace tana {
 
+    class Routine {
+    public:
+        tana_type::T_ADDRESS start_addr; // start address of the function
+        tana_type::T_ADDRESS end_addr;   // end address of the function
+        std::string rtn_name;
+        std::string module_name;
+        tana_type::T_ADDRESS size;
+
+        Routine() : start_addr(0), end_addr(0), rtn_name(), module_name(), size(0) {}
+    };
+
 	class Function {
 	private:
 		std::list<tana::Routine> rtn_libraries;
+		std::set<tana_type::T_ADDRESS> existing_rtn;
 
 
 	public:
-		Function(std::ifstream *function_file);
+		explicit Function(std::ifstream *function_file);
 
 		std::string findTaintedRTN(tana::tana_type::T_ADDRESS addr);
 
