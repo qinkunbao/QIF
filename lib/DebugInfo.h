b44fe791bf4c5ed7d0eb17ad90c47a8260a54645
qinkunbao <qinkunbao@gmail.com>
2019-09-04 15:31:12 -0400

    add debuginfo class

--- lib/DebugInfo.h
+++ lib/DebugInfo.h
@@ -1,6 +1,6 @@
 /*************************************************************************
 	> File Name: DynSEEngine.cpp
-	> Author: Qinkun Bao
+	> Author: Qinkun Bao & Zihao Wang
 	> Mail: qinkunbao@gmail.com
 	> Created Time: Tue Sep  3 17:19:02 EDT 2019
  ************************************************************************/
@@ -9,7 +9,32 @@
 
 
 #include <vector>
+#include <memory>
+#include <string>
+#include <fstream>
+
 
 namespace tana{
+
+    class DebugSymbol{
+    public:
+        std::string pwd;
+        std::string file_name;
+        uint32_t address;
+        int line_number;
+        DebugSymbol(const std::string& fPwd, const std::string& fileName, uint32_t binaryAddress, int lineNumber):
+                   pwd(fPwd), file_name(fileName), address(binaryAddress), line_number(lineNumber){}
+    };
+
+
+    class DebugInfo{
+    private:
+        std::vector<std::shared_ptr<DebugSymbol>> line_info;
+    public:
+        explicit DebugInfo(std::ifstream &debug_file);
+        std::shared_ptr<DebugSymbol> locateSym(uint32_t addr);
+        ~DebugInfo() = default;
+    };
+
 
 }
