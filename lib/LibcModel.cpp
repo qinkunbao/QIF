c98a457aff110269c3be42048103c299d819dbbb
Qinkun Bao <qinkunbao@gmail.com>
2019-09-23 16:47:52 -0400

    update libc model

--- lib/LibcModel.cpp
+++ lib/LibcModel.cpp
@@ -23,6 +23,7 @@ string uint32ToStr(uint32_t addr)
     return sstream.str();
 }
 
+
 bool LIBC_X86_Calloc::symbolic_execution(tana::SEEngine *se)
 {
     assert(!this->is_static);
@@ -38,7 +39,7 @@ bool LIBC_X86_Calloc::symbolic_execution(tana::SEEngine *se)
         auto data = std::make_shared<BitVector>(ValueType::CONCRETE, 0);
         uint32_t addr = ret + i * T_DWORD;
         addr_str = uint32ToStr(addr);
-        se->writeMem(addr_str, T_DWORD, data);
+        se->writeMem(addr_str, T_BYTE, data);
     }
 
     // We think eax register store the return value
@@ -51,7 +52,7 @@ bool LIBC_X86_Calloc::symbolic_execution(tana::SEEngine *se)
 bool LIBC_X86_Malloc::symbolic_execution(tana::SEEngine *se)
 {
     assert(!this->is_static);
-    uint32_t size, ret;
+    uint32_t ret;
     ret = this->m_ret_value;
     auto ret_v = std::make_shared<BitVector>(ValueType::CONCRETE, ret);
     se->writeReg("eax", ret_v);
@@ -62,6 +63,25 @@ bool LIBC_X86_Memcpy::symbolic_execution(tana::SEEngine *se)
 {
     assert(!this->is_static);
     uint32_t destination, source, num, ret;
+    destination = m_destination;
+    source = m_source;
+    num = m_num;
+    ret = m_ret_value;
+    uint32_t index;
+    shared_ptr<BitVector> data;
+    string source_addr_str, dest_addr_str;
+    for(index = 0; index < num; ++index)
+    {
+        source_addr_str = uint32ToStr(source + index );
+        dest_addr_str = uint32ToStr(destination + index);
+
+        data = se->readMem(source_addr_str, T_DWORD);
+
+        se->writeMem(dest_addr_str, T_DWORD, data);
+    }
+
+    auto rev_v = std::make_shared<BitVector>(ValueType ::CONCRETE, destination);
+    se->writeReg("eax", rev_v);
     return true;
 }
 
@@ -69,6 +89,25 @@ bool LIBC_X86_Mempcpy::symbolic_execution(tana::SEEngine *se)
 {
     assert(!this->is_static);
     uint32_t destination, source, num, ret;
+    destination = m_destination;
+    source = m_source;
+    num = m_num;
+    ret = m_ret_value;
+    uint32_t index;
+    shared_ptr<BitVector> data;
+    string source_addr_str, dest_addr_str;
+    for(index = 0; index < num; ++index)
+    {
+        source_addr_str = uint32ToStr(source + index);
+        dest_addr_str = uint32ToStr(destination + index);
+
+        data = se->readMem(source_addr_str, T_DWORD);
+
+        se->writeMem(dest_addr_str, T_DWORD, data);
+    }
+
+    auto rev_v = std::make_shared<BitVector>(ValueType ::CONCRETE, destination + index * T_DWORD);
+    se->writeReg("eax", rev_v);
     return true;
 }
 
@@ -76,6 +115,24 @@ bool LIBC_X86_Memset::symbolic_execution(tana::SEEngine *se)
 {
     assert(!this->is_static);
     uint32_t ptr, value, num, ret;
+    ptr = m_ptr;
+    value = m_value;
+    num = m_num;
+    ret = m_ret_value;
+
+    uint32_t index;
+    string addr_str;
+    shared_ptr<BitVector> data;
+    for(index = 0; index < num; ++index)
+    {
+        data = make_shared<BitVector>(ValueType::CONCRETE, value);
+        addr_str = uint32ToStr(ptr + index);
+        se->writeMem(addr_str, T_BYTE_SIZE, data);
+    }
+
+    auto rev_v = std::make_shared<BitVector>(ValueType ::CONCRETE, ret);
+    se->writeReg("eax", rev_v);
+
     return true;
 }
 
