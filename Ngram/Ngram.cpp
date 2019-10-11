c11337423c6640c89a3984698092c2a1dfea0a94
Qinkun Bao <qinkunbao@gmail.com>
2019-10-10 21:49:09 -0400

    start to work on n-gram

---
+++ Ngram/Ngram.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: Ngram.cpp
+	> Author: Qinkun Bao
+	> Mail: qinkunbao@gmail.com
+	> Created Time: Thu Oct 10 21:46:18 2019
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+using namespace tant;
+
+
+int main(){
+    cout << "Hello world!";
+    return 0;
+}
